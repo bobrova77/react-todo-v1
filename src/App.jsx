3be830adcb9b59348a934eb244715b20956e7f6e
Anna Bobrova <spomandarin@gmail.com>
2024-11-06 21:28:49 -0600

    Added the first part of the code for lesson 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import "./App.css";
+import React from "react";
 
-
-import React from 'react'; 6.9k (gzipped: 2.7k)
-
+// Step 5: Create an empty array with todo items
+const todoList = [
+  { id: 1, title: "Complete assignment" },
+  { id: 2, title: "Do the dishes" },
+  { id: 3, title: "Take out the trash" },
+];
 
 function App() {
-  const element = <h1>My List</h1>;
-  const elem = React.createElement("h2", null, "List");
-  const el = (
-    <div>
-      <h3>App to Do</h3>
-      <input type='text'/>
-    </div>
-  )
-  const list = [
-    {id: 1, title: 'do homework'},
-    {id: 2, title: 'cook a cake'},
-  ]
-
-
   return (
     <div>
+      {/* Step 3: Create a level-one heading */}
+      <h1>Todo List</h1>
+
+      {/* Step 4: Create an unordered list and map over the todoList array */}
       <ul>
-        {list.map((id, title) => (<li key={id}>{title}</li>))}
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-
 export default App;
-
