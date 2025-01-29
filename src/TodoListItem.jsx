a9048d92a277982165df95afe5271473cb5185c7
Anna Bobrova <spomandarin@gmail.com>
2025-01-28 23:29:45 -0600

    Lesson 13 code added.

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,6 +1,7 @@
 // 7.18 Add "Remove" Button to List Items - Add a button element, type "button", inside the list item with text "Remove"
 
 import React from "react";
+import style from "./TodoListItem.module.css"; // Import the CSS module
 
 // 7.19 Remove function TodoListItem({ todo }) {
 // function TodoListItem({ todo }) {
@@ -8,9 +9,12 @@ import React from "react";
 const TodoListItem = ({ todo, onRemoveTodo }) => {
   return (
     //Move list item JSX from TodoList.jsx to TodoListItem.jsx
-    <li>
+    <li className={style.ListItem}>
       {todo.title}
-      <button type="button" onClick={() => onRemoveTodo(todo.id)}>
+      <button
+        className={style.RemoveButton}
+        onClick={() => onRemoveTodo(todo.id)}
+      >
         Remove
       </button>
     </li>
